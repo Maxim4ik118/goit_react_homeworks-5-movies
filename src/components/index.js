3deaa7f7b972b70374094be421324c6b23078400
Maxi <mmaximonchuk@gmail.com>
2022-07-19 20:29:58 +0300

    gh-pages

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as MoviesGallery } from './MoviesGallery/MoviesGallery';
-export { default as Searchbar } from './Searchbar/Searchbar';
-export { default as Loader } from './Loader/Loader';
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as HomePage } from './HomePage';
-export { default as Movies } from './Movies';
-export { default as MovieDetails } from './MovieDetails';
-export { default as Cast } from './Cast';
-export { default as Reviews } from './Reviews';
